cd544b1f46fee4fb1193541b389e2a7cb0aaacf1
n0v4-90 <43476353+n0v4-90@users.noreply.github.com>
2021-02-19 10:50:32 +0100

    Coded in the lecture as part of learning assigment (14.02.2017)

---
+++ legoApp2/legoApp2/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using legoApp2.Models;
+
+namespace legoApp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*
+        Public string index()
+        {
+            Return "<h1>Hei fra controller</h1> "();
+        }
+        */
+
+public ActionResult index()
+        {
+            return View();
+        }
+
+
+        public ActionResult about()
+        {
+            return View();
+        }
+
+        public string WhatIWrote(String id)
+        {
+            return "Du tastet inn: " + id;
+        }
+
+        public ActionResult buy()
+        {
+            return View();
+        }
+        public ActionResult VisAlleLegosett()
+        {
+            List<Legosett> LegosettList = new List<Legosett>
+                {
+                new Legosett { Navn = "harry Potter lekestue", AntallDeler = 130, Pris = 344, Bilde = "hp.jpg"
+                 },
+                 new Legosett
+                 {
+                Navn = "harry Potter lekestue1",
+                AntallDeler = 130,
+                Pris = 344,
+                Bilde = "hp1.jpg"
+                },
+
+                  new Legosett
+                 {
+                Navn = "harry Potter lekestue2",
+                AntallDeler = 130,
+                Pris = 344,
+                Bilde = "hp2.jpg"
+                },
+
+                   new Legosett
+                 {
+                Navn = "harry Potter lekestue3",
+                AntallDeler = 130,
+                Pris = 344,
+                Bilde = "hp3.jpg"
+                }
+
+                };
+
+            return View(LegosettList);
+        }
+
+    }
+}
---
+++ legoApp2/legoApp2/Models/Legosett.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace legoApp2.Models
+{
+    public class Legosett
+    {
+        //Atributter(+ tilgangsmetoder hente verdier ut )
+        
+        public String Navn { get; set; }
+        public int AntallDeler { get; set; }
+        public double Pris { get; set; }
+        public String Bilde { get; set; }
+    }
+}
